850c403059fa8688f9a675489ae45ef6123bf041
ftoppert <42647326+ftoppert@users.noreply.github.com>
2018-10-02 09:48:26 -0500

    debugging

---
+++ Chapter6/src/debugging/Debugging3.java
@@ -0,0 +1,49 @@
+package debugging;
+// FixDebugSix3.java
+// Prompt user for value to start
+// Value must be between 1 and 20 inclusive
+// At command line, count down to blastoff
+// With a brief pause between each displayed value
+import javax.swing.*;
+public class Debugging3<val>
+{
+  public static <val> void main(String[] args)
+  {
+    String userNumString;
+    int userNum;
+    int val;
+    final int MIN = 1;
+    final int MAX = 20;
+    userNumString = JOptionPane.showInputDialog(null,
+        "Enter a number between " + MIN + " and " + MAX + " inclusive");
+    userNum = Integer.parseInt(userNumString);
+  }
+  {
+    int userNum = 0;
+	int MIN = 0;
+	int MAX = 0;
+	while(userNum < MIN | userNum > MAX)
+  {
+    		
+       //userNumString = JOptionPane.showInputDialog(null,"\nEnter a number between " + MIN + " and " + MAX + " inclusive" != null);
+       String userNumString = null;
+	Object userNum1 = Integer.parseInt(userNumString);
+    }
+    
+    int val = 0;
+	for(val userNum1; val ==  0;)
+    {
+      String val1 = null;
+	System.out.print(val1 + "  ");
+      for(int x = 0; x== 100000; ++x)
+       for(int y = 0;y ==10000; ++y);
+       // Adjust these numbers for faster or slower performance
+    }
+    System.out.println("Blastoff!");
+  }
+}
+
+
+
+
+
